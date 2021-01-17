9372d477887e81810a7be50d487d940ec220608c
frlute <frluter@gmail.com>
2021-01-17 15:54:26 +0800

    fix bubble sort improve version flag position bug

--- src/my-sort/BubbleSort.go
+++ src/my-sort/BubbleSort.go
@@ -4,7 +4,9 @@ import "sort"
 
 /*
 排序算法解释:
-
+	1.比较相邻元素
+	2.看是否满足大小关系要求，如果不满足交换位置
+	3.循环 1、2 步骤
 */
 func BubbleSort(data sort.Interface) {
 	n := data.Len()
@@ -21,18 +23,15 @@ func BubbleSort(data sort.Interface) {
 	}
 }
 
-/*
-BubbleSortV1
-
-*/
+// BubbleSortV1 改进：如果某次没有交换时表示已排好序
 func BubbleSortV1(data sort.Interface) {
 	n := data.Len()
 	if n <= 1 {
 		return
 	}
 
-	isChanged := false
 	for i := 0; i < n; i++ {
+		isChanged := false
 		for j := 0; j < n-i-1; j++ {
 			if !data.Less(j, j+1) {
 				data.Swap(j, j+1)
