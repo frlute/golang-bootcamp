4a929b82187bb3c4705937987b72088160f12c31
frlute <frluter@gmail.com>
2021-04-20 16:33:49 +0800

    adjust DataStructure related content

--- cmd/fibonacci/main.go
+++ cmd/fibonacci/main.go
@@ -12,9 +12,27 @@ func fibonacci() func() int {
 	}
 }
 
-func main() {
-	f := fibonacci()
-	for i := 0; i < 10; i++ {
-		fmt.Println(f())
+func waysToStep(n int) int {
+	if n == 1 {
+		return 1
 	}
+	if n == 2 {
+		return 2
+	}
+	if n == 3 {
+		return 4
+	}
+	dp1, dp2, dp3 := 1, 2, 4
+	for i := 4; i <= n; i++ {
+		dp1, dp2, dp3 = dp2, dp3, (dp1+dp2+dp3)%1000000007
+	}
+	return dp3
+}
+
+func main() {
+	// f := fibonacci()
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(f())
+	// }
+	fmt.Println(waysToStep(11))
 }
