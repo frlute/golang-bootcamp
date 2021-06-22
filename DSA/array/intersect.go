7ae49631f5d84a898d7a3e4e01e48dc77e5f8511
frlute <frluter@gmail.com>
2021-06-22 10:45:39 +0800

    add array intersect implement

---
+++ DSA/array/intersect.go
@@ -0,0 +1,29 @@
+package array
+
+/*
+计算两个已排序数组的交集,
+要求：
+	1. 两个数组有重复元素时，交集中不允许出现重复数字
+*/
+
+func intersect(a, b []int) []int {
+	posA, posB := 0, 0
+	intersection := []int{}
+
+	for posA < len(a) && posB < len(b) {
+		if a[posA] == b[posB] {
+			// 避免重复元素多次追加
+			if posA == 0 || a[posA] != a[posA-1] {
+				intersection = append(intersection, a[posA])
+			}
+			posA++
+			posB++
+		} else if a[posA] > b[posB] {
+			posB++
+		} else if a[posA] < b[posB] {
+			posA++
+		}
+	}
+
+	return intersection
+}
