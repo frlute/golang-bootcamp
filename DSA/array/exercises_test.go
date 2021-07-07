4009626907ea3463e4bd6f8a4d3fdc09bae36745
frlute <frluter@gmail.com>
2021-07-07 22:58:36 +0800

    all array exercises migrated to exercised file

--- DSA/array/exercises_test.go
+++ DSA/array/exercises_test.go
@@ -6,6 +6,67 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func Test_evenOdd(t *testing.T) {
+	// TODO 规范测试用例
+	assert.Equal(t, []int{2, 4, 8, 3, 9, 5, 1}, evenOdd([]int{1, 5, 8, 3, 4, 9, 2}))
+}
+
+type inputArgs struct {
+	A []int
+	B []int
+}
+
+func Test_intersect(t *testing.T) {
+	testCases := []struct {
+		args   inputArgs
+		expect []int
+	}{
+		{
+			args: inputArgs{
+				A: []int{2, 3, 3, 5, 7, 11},
+				B: []int{3, 3, 7, 15, 31},
+			},
+			expect: []int{3, 7},
+		},
+	}
+
+	for _, c := range testCases {
+		actual := intersect(c.args.A, c.args.B)
+		assert.Equal(t, c.expect, actual)
+	}
+}
+
+func Test_twoSum(t *testing.T) {
+	testCases := []struct {
+		input  []int
+		target int
+		output []int
+	}{
+		{
+			input:  []int{1},
+			target: 5,
+			output: nil,
+		},
+		{
+			input:  []int{1, 2, 5, 6, 8, 10},
+			target: 13,
+			output: []int{2, 4},
+		},
+	}
+
+	for _, item := range testCases {
+		t.Run("test two sum", func(t *testing.T) {
+			actual := twoSum(item.input, item.target)
+			assert.Equal(t, item.output, actual)
+
+			assert.Equal(t, item.output, twoSumBasedHash(item.input, item.target))
+
+			// 此解法数组必须已排序
+			assert.Equal(t, item.output, twoSumBasedInvariants(item.input, item.target))
+		})
+	}
+}
+
 func Test_removeElement(t *testing.T) {
 	type Case struct {
 		name   string
